4335f7f541b3c07fdcdd67ae1b6fc006376b94c4
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-16 14:31:16 +0600

    (Advanced) Understand the concept of use

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Counter from './counter'
 import Batsman from './Batsman'
 import Bolwer from './Bowler'
+import Users from './users'
 import './App.css'
 
 function App() {
-  
+
   function handelClick() {
     alert('I am clicked.')
   }
-  
-  const  handelClick3 = () => {
+
+  const handelClick3 = () => {
     alert('clicked 3')
-  } 
+  }
 
   const handleAdd5 = (num) => {
     const newNum = num + 5;
@@ -20,21 +21,24 @@ function App() {
 
   return (
     <>
-      
+
       <h3>Vite + React</h3>
-     <Bolwer></Bolwer>
+
+      <Users></Users>
+      
+      <Bolwer></Bolwer>
       <Batsman></Batsman>
       <Counter></Counter>
 
       {/* <button onClick="handelClick()">Click me</button> */}
       <button onClick={handelClick}>Click me</button>
-      <button onClick={function handelClick2(){
+      <button onClick={function handelClick2() {
         alert('Clicked 2')
       }}>Click me2</button>
       <button onClick={handelClick3}>Clicked me 3</button>
 
       <button onClick={() => alert('click 4')}>Clicked me 4</button>
-      
+
       <button onClick={() => handleAdd5(10)}>Click Add 5</button>
     </>
   )
